3994621814a569ec4b58a9f991c6aaa7cf6f72cd
Adam Dickmeiss <adam@indexdata.dk>
2014-10-08 09:09:22 +0200

    mkdist does not read debian/changelog anymore

--- mkdist.sh
+++ mkdist.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 set -x
-DEBCHANGELOG=debian/changelog
-
-l=`head -1 $DEBCHANGELOG`
-NAME=`echo $l|sed 's/ .*//g'`
-FULLVERSION=`echo $l|sed 's/.*(//g'|sed 's/).*//g'`
-DEBVERSION=`echo $FULLVERSION|sed 's/-.*//g'`
 . ./IDMETA
-if test "$DEBVERSION" != "$VERSION"; then
-    echo "WARNING: File VERSION and debian/changelog do not match"
-    sleep 2
-fi
 git log >ChangeLog
 git archive --format=tar --prefix=$NAME-$VERSION/ HEAD > $NAME-$VERSION.tar
 tar xf $NAME-$VERSION.tar
 cp ChangeLog $NAME-$VERSION
-cd  $NAME-$VERSION
+cd $NAME-$VERSION
 rm -fr debian
 cd ..
 tar cfz $NAME-$VERSION.tar.gz $NAME-$VERSION
