62e13b7a372c8efb7ca095f0871f0a4df5ed821a
Adam Dickmeiss <adam@indexdata.dk>
2026-09-07 15:43:46 +0200

    Version 1.2.5 PIE (#32)

--- php_yaz.h
+++ php_yaz.h
@@ -1,12 +1,12 @@
 /* This file is part of PHP YAZ.
- * Copyright (C) Index Data 2004-2018
+ * Copyright (C) Index Data 2004-2026
  * See the file LICENSE for details.
  */
 
 #ifndef PHP_YAZ_H
 #define PHP_YAZ_H
 
-#define PHP_YAZ_VERSION "1.2.4"
+#define PHP_YAZ_VERSION "1.2.5"
 
 #if HAVE_YAZ
 
